0cc90b13475d4dc3ad290713f86891936896b5ef
Aki Salmi <aki.salmi@iki.fi>
2018-08-25 19:58:50 +0200

    less variables

--- src/rover.js
+++ src/rover.js
@@ -1,13 +1,14 @@
+function forward(facing) {
+  if (facing === 's')
+    return ([x, y]) => [x, y - 1]
+  else
+    return ([x, y]) => [x, y + 1]
+}
+
 module.exports = mission => {
   const [x, y, facing, commands] = mission; // variables
-  let forward;
-  if ( facing === 's' )
-    forward = ([x, y]) => [x, y - 1]
-  else
-    forward = ([x, y]) => [x, y + 1]
-
   if (commands === 'f')
-    return forward([x,y]);
+    return forward(facing)([x,y]);
   else
     return [x, y];
 };
